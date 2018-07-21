c1af296a76206fc63ae32668d253815cbf49e194
clei1 <callmecon328@gmail.com>
2018-07-21 14:39:58 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,5 +3,13 @@
 def least_coins(cents)
 
 #Code your answer here!
-
+#probably could have used float
+  c = cents
+  quarters = (c - c % 25) / 25
+  c -= (quarters * 25)
+  dimes = (c - c % 10) / 10
+  c -= (dimes * 10)
+  nickels = (c - c % 5) / 5
+  c -= (nickels * 5)
+  return {"quarters": quarters, "dimes": dimes, "nickels": nickels, "pennies": c}
 end
